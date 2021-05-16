1b3e268476eb34753de4a9e6959b8c9c656399c6
gyanpra <gyanp008@gmail.com>
2021-05-16 11:20:57 +0530

    Add project files.

---
+++ Controllers/ManageProductsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineKirana;
+using OnlineKirana.Models;
+
+namespace OnlineKirana.Controllers
+{
+    public class ManageProductsController : Controller
+    {
+        private OnlineKiranaEntities db = new OnlineKiranaEntities();
+        // GET: ManageProducts
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductName,Category,Brand,Price,QualityOnHand,ReOrderLevel")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,Name,Category,Brand,imagePath,Sex,Price,StockInDate")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Images(int id)
+        {
+            var pro = db.Products.Where(l => l.ProductID == id).ToList();
+            if (pro == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.pro = pro;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Images(int id, HttpPostedFileBase file)
+        {
+            string path = System.IO.Path.Combine("~/Content/Images/"+ file.FileName);
+            file.SaveAs(Server.MapPath(path));
+            
+            return View("Index");
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using OnlineKirana.Models;
+
+namespace OnlineKirana.Controllers
+{
+    public class ProductController : Controller
+    {
+        private OnlineKiranaEntities db = new OnlineKiranaEntities();
+        // GET: Product
+        public ActionResult Index(string SearchBy, string search)
+        {
+            if (SearchBy == "Brand")
+            {
+                return View(db.Products.Where(x => x.Brand == search || search == null).ToList());
+            }
+            else if (SearchBy == "Category")
+            {
+                return View(db.Products.Where(x => x.Category == search || search == null).ToList());
+            }
+            else
+            {
+                return View(db.Products.Where(x => x.ProductName.StartsWith(search) || search == null).ToList());
+            }
+        }
+    }
+}
